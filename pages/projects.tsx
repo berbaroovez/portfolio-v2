5f332636383847bc5e6bbd936a18bfb7df431c76
carter <carterolson3@gmail.com>
2021-09-13 15:10:50 -0500

    fixed spelling erros

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -13,11 +13,11 @@ export default function Projects() {
           title: "projects – Carter Olson",
         }}
       />
-      <PageTitles title="Projects" subtitle="yes there is spelling mistakes" />
+      <PageTitles title="Projects" subtitle="💻" />
       <ProjectBox>
         <ProjectTitle>Task Manager</ProjectTitle>
         <p>
-          Live streamer Nathan Stanz had been watching a old birtish show called{" "}
+          Live streamer Nathan Stanz had been watching a old british show called{" "}
           <span>Task Master </span> on stream and wanted to put on his own
           version of this show for his community. He wanted a website to create
           weekly task that his audience would have to complete and submit for
@@ -46,14 +46,14 @@ export default function Projects() {
           We used a service provided by a company called Virgil Security to do
           end to end encryption. The site was built using NextJS and hosted on
           Vercel for easy deployments. My duties included doing a majority of
-          the front end work, and ensuring the enecryption was working.
+          the front end work, and ensuring the encryption was working.
         </p>
 
         <p>
           I learned a lot from this project especially since it was my first
           project with a team and that had a paying customer. I had to decrease
-          loading times on the admin dashboard by 90% by not unencrypting the
-          photos until the were shown. Adding feautures such as exporting data
+          loading times on the admin dashboard by 90% by not decrypting the
+          photos until the were shown. Adding features such as exporting data
           via CSV or adding the ability to create a safelist of allowed emails
           that could sign up.
         </p>
@@ -65,7 +65,7 @@ export default function Projects() {
           of 2021 their valorant team advanced to the first ever LAN for the
           game valorant. As one of underdog teams they came in and won their
           first game with their star player wearing cat ears on his headset.
-          Thinking quickly i launced a website where people can upload photos
+          Thinking quickly i launched a website where people can upload photos
           and add cat eats to it then download it. I then tweeted at the
           org/posted in their discord.
         </p>
@@ -93,7 +93,7 @@ export default function Projects() {
           simple website made for my mother who does custom sugar cookies. A few
           times a year she does cookie drops and needed a better way to fulfil
           orders. Created a simple website that allow her to upload her
-          spreadsheet and see the info in seperate cards. First time storing
+          spreadsheet and see the info in separate cards. First time storing
           data in local storage allowing for the website to be closed and
           reopened to the same info.
         </p>
@@ -102,8 +102,8 @@ export default function Projects() {
         <ProjectTitle>Cacti</ProjectTitle>
         <p>
           <a href="https://drinkcacti.vercel.app/"> Cacti </a>, This was just a
-          remake of the website used to launch the seltzier brand cacti. I was
-          practing styling and this website seemed unique and simple enough to
+          remake of the website used to launch the seltzer brand cacti. I was
+          practicing styling and this website seemed unique and simple enough to
           copy.
         </p>
       </ProjectBox>
@@ -111,15 +111,15 @@ export default function Projects() {
         <ProjectTitle>Cookie Crumbs</ProjectTitle>
         <p>
           <a href="https://cookiecrumbs.vercel.app/"> Cookie Crumbs </a> . Oh
-          boy this project taught me alot and i failed a ton. it was an attempt
-          to create an one stop shop for my moms cookie buisness. ordering
+          boy this project taught me a lot and i failed a ton. it was an attempt
+          to create an one stop shop for my moms cookie business. ordering
           forms, a backend to accept/deny orders, an order tracker to see what
           stage a order was in, and even a payment system to accept payments.
         </p>
         <p>
           This was my first major project that had a real world goal in mind. I
           learned a lot about when to use server side rendering vs client side
-          rendering with NextJS. The diffuclty of creating a order form that
+          rendering with NextJS. The difficulty of creating a order form that
           worked for multiple people. I even attempted at creating a form maker
           which i failed at.
         </p>
