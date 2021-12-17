ffe18fc53a83ab4656cc124663330100d5bdcc7f
berbaroovez <carterolson3@gmail.com>
2021-12-17 17:50:35 -0600

    r3d homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
+import FallingSquares from "../components/r3f/FallingSquares";
+import { Cube } from "../components/r3f/Shapes";
 
 const Title = styled.h1`
   font-size: 50px;
@@ -6,21 +9,28 @@ const Title = styled.h1`
 
 export default function Home() {
   return (
-    <IndexDiv>
-      <h1>car·ter ol·son</h1>
-      <p>KARR-ter OAL-sen</p>
-      <p className="def">A web developer focused on Creators and Web3. </p>
-      <p className="aboutme">
-        I love creating web apps that help creators connect with their audience
-        better. Allowing them to grow their community is my main goal. More
-        recently I've been exploring Ethereum and Solidity in the goal of
-        getting a job in that field. Web3 technologies really intrigue me
-        because they are one the natural payment layer of the internet and
-        secondly because they allow the user to own their data.
-      </p>
-    </IndexDiv>
+    <Indexzone>
+      <IndexDiv>
+        <h1>car·ter ol·son</h1>
+        <p>KARR-ter OAL-sen</p>
+        <p className="def">A web developer focused on Creators and Web3. </p>
+        <p className="aboutme">
+          I love creating web apps that help creators connect with their
+          audience better. Allowing them to grow their community is my main
+          goal. More recently I've been exploring Ethereum and Solidity in the
+          goal of getting a job in that field. Web3 technologies really intrigue
+          me because they are one the natural payment layer of the internet and
+          secondly because they allow the user to own their data.
+        </p>
+      </IndexDiv>
+      <FallingSquares />
+    </Indexzone>
   );
 }
+
+const Indexzone = styled.div`
+  overflow: hidden;
+`;
 
 const IndexDiv = styled.div`
   svg {
